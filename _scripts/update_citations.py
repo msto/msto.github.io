20be7879138b056c566ffe4f23e702af595fa418
Matt Stone <matthew.stone12@gmail.com>
2017-09-12 21:49:37 -0400

    update pubs

--- _scripts/update_citations.py
+++ _scripts/update_citations.py
@@ -8,33 +8,170 @@
 
 """
 
-import argparse
-from _scripts.parse_pubmed import scrape_pubmed
+import itertools
+from datetime import datetime
+from Bio import Entrez
+
+
+def parse_pubmed_date(article):
+    """
+    citation : Bio.Entrez.Parser.CitationElement
+    """
+    issue = article['MedlineCitation']['Article']['Journal']['JournalIssue']
+    pub_date = issue['PubDate']
+
+    if 'Day' not in pub_date:
+        fmt = "{year} {month}"
+        day = None
+    else:
+        fmt = "{year} {month} {day}"
+        day = issue['PubDate']['Day']
+
+    return fmt.format(year=issue['PubDate']['Year'],
+                      month=issue['PubDate']['Month'],
+                      day=day)
+
+
+def parse_pubmed_issue(citation):
+    """
+    citation : Bio.Entrez.Parser.CitationElement
+    """
+    issue = citation['Journal']['JournalIssue']
+
+    fmt = "{volume}({issue}):{page}"
+
+    return fmt.format(volume=issue['Volume'],
+                      issue=issue['Issue'],
+                      page=citation['Pagination']['MedlinePgn'])
+
+
+def format_pubmed_citation(article):
+    """
+    article : Bio.Entrez.Parser.DictionaryElement
+    """
+
+    # Parse doi
+    for data in article['PubmedData']['ArticleIdList']:
+        if data.attributes['IdType'] == 'doi':
+            doi = str(data)
+            doi = '<a href="https://doi.org/{0}">{0}</a>'.format(doi)
+        elif data.attributes['IdType'] == 'pubmed':
+            fmt = '<a href="https://www.ncbi.nlm.nih.gov/pubmed/{0}">{0}</a>'
+            pmid = fmt.format(str(data))
+
+    # get article data
+    citation = article['MedlineCitation']['Article']
+
+    def _parse_author(author):
+        name = author['LastName'] + ' ' + author['Initials']
+        if name == "Stone MR":
+            name = "<strong>{0}</strong>".format(name)
+        return name
+
+    authors = [_parse_author(author) for author in citation['AuthorList']]
+    authors = ', '.join(authors)
+
+    #  title = citation['ArticleTitle'].replace(u'\xa0', ' ').rstrip('.')
+    title = citation['ArticleTitle'].rstrip('.')
+    journal = citation['Journal']['ISOAbbreviation'].replace('.', '')
+
+    date = parse_pubmed_date(article)
+    issue = parse_pubmed_issue(citation)
+
+    # note: trailing doublespace necessary for line break in markdown
+    citation = ("{authors}. {title}.<br>  \n"
+                "<em>{journal}</em>. {date};{issue}.<br>  \nPMID: {pmid}. DOI: {doi}.")
+    citation = citation.format(**locals())
+
+    return citation
+
+
+def scrape_pubmed(idlist):
+    """
+    idlist : list of str
+        pubmed IDs
+    """
+    Entrez.email = 'matthew.stone12@gmail.com'
+    handle = Entrez.efetch(db='pubmed', retmode='xml', id=','.join(idlist))
+    results = Entrez.read(handle)
+    articles = results['PubmedArticle']
+
+    def _timestamp(article):
+        date = parse_pubmed_date(article)
+        if len(date.split()) == 3:
+            return datetime.strptime(parse_pubmed_date(article), '%Y %b %d')
+        else:
+            return datetime.strptime(parse_pubmed_date(article), '%Y %b')
+
+    articles = sorted(articles, key=lambda a: _timestamp(a))[::-1]
+
+    citations = [format_pubmed_citation(article) for article in articles]
+
+    return citations
+
+
+def get_year(citation):
+    return citation.split('</em>. ')[1].split()[0]
+    #  return citation.split('\n')[1].split('_. ')[1].split()[0]
+
+
+def write_citations(citations, fout):
+    idx = len(citations)
+    citations = sorted(citations, key=get_year)[::-1]
+
+    for year, cites in itertools.groupby(citations, get_year):
+        fout.write('### {year}\n'.format(year=year))
+        #  fout.write('{{:start="{0}"}}\n'.format(idx))
+        fout.write('<ol start="{0}" reversed>\n'.format(idx))
+
+        citelist = []
+        for i, cite in enumerate(cites):
+            cite = '<li>{0}</li>\n'.format(cite)
+            #  cite = '<li>{0}</li><br>\n'.format(cite)
+            idx -= 1
+            citelist.append(cite)
+
+        cites = '\n'.join(citelist)
+        fout.write(cites)
+        fout.write('</ol>\n\n')
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('pubmed_ids', type=argparse.FileType('r'))
-    parser.add_argument('citations', type=argparse.FileType('w'))
-    args = parser.parse_args()
+    #  parser = argparse.ArgumentParser(
+        #  description=__doc__,
+        #  formatter_class=argparse.RawDescriptionHelpFormatter)
+    #  parser.add_argument('pubmed_ids', type=argparse.FileType('r'))
+    #  parser.add_argument('citations', type=argparse.FileType('w'))
+    #  args = parser.parse_args()
 
-    args.citations.write("---\nlayout: page\ntitle: Publications & Preprints\n"
-                         "permalink: /publications/\n---\n")
+    PUBMED_LIST = open("_data/pubmed_ids.list")
+    pubmed_ids = [p.strip() for p in PUBMED_LIST.readlines()]
 
-    pmids = [p.strip() for p in args.pubmed_ids.readlines()]
-    citations = scrape_pubmed(pmids)
+    # terrible don't do any of this
+    BIORXIV_LIST = open("_data/biorxiv_citations.html")
+    biorxiv_citations = []
+    for line in BIORXIV_LIST:
+        author_title = line.strip()
+        journal_date = next(BIORXIV_LIST).strip()
+        doi = next(BIORXIV_LIST).strip()
+        citation = '<br>\n'.join([author_title, journal_date, doi])
+        biorxiv_citations.append(citation)
+        try:
+            next(BIORXIV_LIST)
+        except:
+            continue
 
-    for i, citation in enumerate(citations):
-        idx = len(citations) - i
-        citation = "{0}. {1}".format(idx, citation)
-        if i < len(citations) - 1:
-            args.citations.write(citation + '\n\n')
-        else:
-            args.citations.write(citation + '\n')
+    CITATION_PAGE = open("publications.md", 'w')
 
-    args.citations.write('{: reversed="reversed"}')
+    CITATION_PAGE.write("---\n"
+                        "layout: page\n"
+                        "title: Publications\n"
+                        "permalink: /publications/\n"
+                        "---\n")
+
+    citations = scrape_pubmed(pubmed_ids)
+    citations = citations + biorxiv_citations
+    write_citations(citations, CITATION_PAGE)
 
 
 if __name__ == '__main__':
---
+++ publications.md
@@ -0,0 +1,45 @@
+---
+layout: page
+title: Publications
+permalink: /publications/
+---
+### 2017
+<ol start="7" reversed>
+<li>Werling DM*, Brand H*, An JY*, <strong>Stone MR</strong>*, Glessner JT*, Zhu L, Collins RL, Dong S, Layer RM, Markenscoff-Papadimitriou E, Farrell A, Schwartz GB, Currall BB, Dea J, Duhn C, Erdman C, Gilson M, Handsaker RE, Kashin S, Klei L, Mandell JD, Nowakowski TJ, Liu Y, Pochareddy S, Smith L, Walker MF, Wang HZ, Waterman MJ, He X, Kriegstein AR, Rubenstein JL, Sestan N, McCarroll SA, Neale BM, Coon H, Willsey AJ, Buxbaum JD, Daly MJ, State MW, Quinlan AR, Marth GT, Roeder K, Devlin B, Talkowski ME, Sanders SJ. Limited contribution of rare, noncoding variation to autism spectrum disorder from sequencing of 2,076 genomes in quartet families.<br>
+<em>bioRxiv</em>. 2017 Apr 13.<br>
+DOI: <a href="https:doi.org/10.1101/127043">10.1101/127043</a></li>
+
+<li>Redin C, Brand H, Collins RL, Kammin T, Mitchell E, Hodge JC, Hanscom C, Pillalamarri V, Seabra CM, Abbott MA, Abdul-Rahman OA, Aberg E, Adley R, Alcaraz-Estrada SL, Alkuraya FS, An Y, Anderson MA, Antolik C, Anyane-Yeboa K, Atkin JF, Bartell T, Bernstein JA, Beyer E, Blumenthal I, Bongers EM, Brilstra EH, Brown CW, Brüggenwirth HT, Callewaert B, Chiang C, Corning K, Cox H, Cuppen E, Currall BB, Cushing T, David D, Deardorff MA, Dheedene A, D'Hooghe M, de Vries BB, Earl DL, Ferguson HL, Fisher H, FitzPatrick DR, Gerrol P, Giachino D, Glessner JT, Gliem T, Grady M, Graham BH, Griffis C, Gripp KW, Gropman AL, Hanson-Kahn A, Harris DJ, Hayden MA, Hill R, Hochstenbach R, Hoffman JD, Hopkin RJ, Hubshman MW, Innes AM, Irons M, Irving M, Jacobsen JC, Janssens S, Jewett T, Johnson JP, Jongmans MC, Kahler SG, Koolen DA, Korzelius J, Kroisel PM, Lacassie Y, Lawless W, Lemyre E, Leppig K, Levin AV, Li H, Li H, Liao EC, Lim C, Lose EJ, Lucente D, Macera MJ, Manavalan P, Mandrile G, Marcelis CL, Margolin L, Mason T, Masser-Frye D, McClellan MW, Mendoza CJ, Menten B, Middelkamp S, Mikami LR, Moe E, Mohammed S, Mononen T, Mortenson ME, Moya G, Nieuwint AW, Ordulu Z, Parkash S, Pauker SP, Pereira S, Perrin D, Phelan K, Aguilar RE, Poddighe PJ, Pregno G, Raskin S, Reis L, Rhead W, Rita D, Renkens I, Roelens F, Ruliera J, Rump P, Schilit SL, Shaheen R, Sparkes R, Spiegel E, Stevens B, <strong>Stone MR</strong>, Tagoe J, Thakuria JV, van Bon BW, van de Kamp J, van Der Burgt I, van Essen T, van Ravenswaaij-Arts CM, van Roosmalen MJ, Vergult S, Volker-Touw CM, Warburton DP, Waterman MJ, Wiley S, Wilson A, Yerena-de Vega MC, Zori RT, Levy B, Brunner HG, de Leeuw N, Kloosterman WP, Thorland EC, Morton CC, Gusella JF, Talkowski ME. The genomic landscape of balanced cytogenetic abnormalities associated with human congenital anomalies.<br>  
+<em>Nat Genet</em>. 2017 Jan;49(1):36-45.<br>  
+PMID: <a href="https://www.ncbi.nlm.nih.gov/pubmed/27841880">27841880</a>. DOI: <a href="https://doi.org/10.1038/ng.3720">10.1038/ng.3720</a>.</li>
+
+<li>Collins RL, Brand H, Redin CE, Hanscom C, Antolik C, <strong>Stone MR</strong>, Glessner JT, Mason T, Pregno G, Dorrani N, Mandrile G, Giachino D, Perrin D, Walsh C, Cipicchio M, Costello M, Stortchevoi A, An JY, Currall BB, Seabra CM, Ragavendran A, Margolin L, Martinez-Agosto JA, Lucente D, Levy B, Sanders SJ, Wapner RJ, Quintero-Rivera F, Kloosterman W, Talkowski ME. Defining the diverse spectrum of inversions, complex structural variation, and chromothripsis in the morbid human genome.<br>  
+<em>Genome Biol</em>. 2017 Mar 06;18(1):36.<br>  
+PMID: <a href="https://www.ncbi.nlm.nih.gov/pubmed/28260531">28260531</a>. DOI: <a href="https://doi.org/10.1186/s13059-017-1158-6">10.1186/s13059-017-1158-6</a>.</li>
+</ol>
+
+### 2016
+<ol start="4" reversed>
+<li>Collins RL, <strong>Stone MR</strong>, Brand H, Glessner JT, Talkowski ME. CNView: a visualization and annotation tool for copy number variation from whole-genome sequencing.<br>
+<em>bioRxiv</em>. 2016 Apr 20.<br>
+DOI: <a href="https://doi.org/10.1101/049536">10.1101/049536</a></li>
+</ol>
+
+### 2015
+<ol start="3" reversed>
+<li>Brand H, Collins RL, Hanscom C, Rosenfeld JA, Pillalamarri V, <strong>Stone MR</strong>, Kelley F, Mason T, Margolin L, Eggert S, Mitchell E, Hodge JC, Gusella JF, Sanders SJ, Talkowski ME. Paired-Duplication Signatures Mark Cryptic Inversions and Other Complex Structural Variation.<br>  
+<em>Am J Hum Genet</em>. 2015 Jul 02;97(1):170-6.<br>  
+PMID: <a href="https://www.ncbi.nlm.nih.gov/pubmed/26094575">26094575</a>. DOI: <a href="https://doi.org/10.1016/j.ajhg.2015.05.012">10.1016/j.ajhg.2015.05.012</a>.</li>
+
+<li>Durst R, Sauls K, Peal DS, deVlaming A, Toomer K, Leyne M, Salani M, Talkowski ME, Brand H, Perrocheau M, Simpson C, Jett C, <strong>Stone MR</strong>, Charles F, Chiang C, Lynch SN, Bouatia-Naji N, Delling FN, Freed LA, Tribouilloy C, Le Tourneau T, LeMarec H, Fernandez-Friera L, Solis J, Trujillano D, Ossowski S, Estivill X, Dina C, Bruneval P, Chester A, Schott JJ, Irvine KD, Mao Y, Wessels A, Motiwala T, Puceat M, Tsukasaki Y, Menick DR, Kasiganesan H, Nie X, Broome AM, Williams K, Johnson A, Markwald RR, Jeunemaitre X, Hagege A, Levine RA, Milan DJ, Norris RA, Slaugenhaupt SA. Mutations in DCHS1 cause mitral valve prolapse.<br>  
+<em>Nature</em>. 2015 Sep 03;525(7567):109-13.<br>  
+PMID: <a href="https://www.ncbi.nlm.nih.gov/pubmed/26258302">26258302</a>. DOI: <a href="https://doi.org/10.1038/nature14670">10.1038/nature14670</a>.</li>
+</ol>
+
+### 2014
+<ol start="1" reversed>
+<li>Brand H, Pillalamarri V, Collins RL, Eggert S, O'Dushlaine C, Braaten EB, <strong>Stone MR</strong>, Chambert K, Doty ND, Hanscom C, Rosenfeld JA, Ditmars H, Blais J, Mills R, Lee C, Gusella JF, McCarroll S, Smoller JW, Talkowski ME, Doyle AE. Cryptic and complex chromosomal aberrations in early-onset neuropsychiatric disorders.<br>  
+<em>Am J Hum Genet</em>. 2014 Oct 02;95(4):454-61.<br>  
+PMID: <a href="https://www.ncbi.nlm.nih.gov/pubmed/25279985">25279985</a>. DOI: <a href="https://doi.org/10.1016/j.ajhg.2014.09.005">10.1016/j.ajhg.2014.09.005</a>.</li>
+</ol>
+
